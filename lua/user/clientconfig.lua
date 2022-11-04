f4957b3179a34ae6704d81d25b0233c382abc321
glubbfreund <oliver.leis@gmail.com>
2022-11-04 01:02:24 +0100

    reconfigured font settings and renamed transparency to clientconfig.lua

--- lua/user/clientconfig.lua
+++ lua/user/clientconfig.lua
@@ -1,10 +1,11 @@
 if vim.g.neovide then
-    vim.g.neovide_transparency = 0.70
+    --vim.g.neovide_transparency = 0.70
     --vim.g.neovide_cursor_vfx_mode = "pixiedust"
-    --vim.api.nvim_exec([[set guifont=Jetbrains\ Mono:h14]], false)
+    vim.g.neovide_fullscreen = true
+    vim.api.nvim_exec([[set guifont=JetBrainsMono\ Nerd\ Font\ Mono:h15]], false)
 else
     require("transparent").setup({
-        enable = false, -- boolean: enable transparent
+        enable = true, -- boolean: enable transparent
         extra_groups = { -- table/string: additional groups that should be cleared
             -- In particular, when you set it to 'all', that means all available groups
             "StatusLineNC",
@@ -19,9 +20,9 @@ else
             "BufferLineBackground",
             "BufferLineSeparator",
             "BufferLineIndicatorSelected", -- table: groups you don't want to clear
-        } 
+        }
     })
-    
+
 end
 
 
