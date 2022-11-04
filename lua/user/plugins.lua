81907c816541530bf049067b6dcb452426836fb2
glubbfreund <oliver.leis@gmail.com>
2022-11-04 09:29:32 +0100

    removed pluigin better escape

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -58,8 +58,7 @@ return packer.startup(function(use)
   use { "ahmedkhalf/project.nvim" }
   use { "lewis6991/impatient.nvim" }
   use { "lukas-reineke/indent-blankline.nvim" }
-  use { "goolord/alpha-nvim" } 
-  use { "max397574/better-escape.nvim" }
+  use { "goolord/alpha-nvim" }
 
   -- Colorschemes
   use { "folke/tokyonight.nvim" }
