18e999d7697fc905a96d4f01dc4fb78aa72ed7ae
Oliver Leis <oliver.leis@gmail.com>
2023-06-30 09:32:39 +0200

    edited netrw options

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -36,5 +36,6 @@ vim.opt.sidescrolloff = 8                       -- minimal number of screen colu
 vim.opt.fillchars.eob=" "                       -- show empty lines at the end of a buffer as ` ` {default `~`}
 vim.opt.shortmess:append "c"                    -- hide all the completion messages, e.g. "-- XXX completion (YYY)", "match 1 of 2", "The only match", "Pattern not found"
 vim.opt.whichwrap:append("<,>,[,],h,l")         -- keys allowed to move to the previous/next line when the beginning/end of line is reached
-vim.g.netrw_banner = 0
-vim.g.netrw_keepdir = 0
+vim.g.netrw_banner = 0                          -- remove banner at the top
+vim.g.netrw_keepdir = 0                         -- keep cwd
+vim.g.netrw_liststyle = 2                       -- 0 - thin, 1 - details 2 - wide, 3 - tree
